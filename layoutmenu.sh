a765a6587b84b9d0aee2c901b9108733c196c70e
Codera <coderem00bfff@gmail.com>
2020-11-27 22:50:23 +0300

    Backup commit 2

---
+++ layoutmenu.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+cat <<EOF | xmenu
+[\\] Dwindle 0
+[]= Default Layout 1 
+[M] Monocle Layout 2
+[@] Spiral 3
+H[] Deck 5
+TTT Bottom Stack 6
+=== Bottom Stack Horizontal 7
+HHH Grid 8
+### Nrow Grid 9
+--- Horizonta Grid 10
+::: Gapples Grid 11
+|M| Centered Master 12
+>M> Centered Floating Master 13
+><> Floating 14
+EOF
