9ca24f357877b1a469ce536eaed0c1bda8627e16
Codera <coderem00bfff@gmail.com>
2020-11-30 19:26:18 +0300

    Update configs a bit

--- config.h
+++ config.h
@@ -1,21 +1,22 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 15;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 25;       /* vert inner gap between windows */
+static const unsigned int gappiv    = 20;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 15;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 25;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
 static const unsigned int swallowfloating = 1;
-static       int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
+static       int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int user_bh            = 30;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const int vertpad            = 8;       /* vertical padding of bar */
-static const int sidepad            = 6;       /* horizontal padding of bar */
+static const int vertpad            = 12;       /* vertical padding of bar */
+static const int sidepad            = 12;       /* horizontal padding of bar */
 static const char *fonts[]          = { "Iosevka:size=12",
-                                        "FontAwesome:size=14",};
+                                        "FontAwesome:size=14",
+                                        "Noto Emoji:size=14",};
 static const char dmenufont[]       = "Iosevka:size=12";
 static const char nord0[]       = "#2e3440";
 static const char nord1[]       = "#3b4252";
@@ -41,7 +42,7 @@ static const char *colors[][3]      = {
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-static const char *tagsalt[] = { "一", "二", "三", "四", "五", "六", "七", "八", "九" };
+static const char *tagsalt[] = { "", "", "", "", "", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -50,7 +51,13 @@ static const Rule rules[] = {
 	 */
 	/* class     instance  title        tags mask      isfloating     isterminal      noswallow      monitor */
 	{ "Gimp",       NULL,   NULL,       0,              1,            0,               0,           -1 },
-  { "Alacritty",  NULL,   NULL,       1 << 1,         0,            1,               0,           -1 },
+  { "TelegramDesktop", NULL, NULL,    1 << 3,         0,            0,               0,           -1 },
+  { "discord",    NULL,   NULL,       1 << 3,         0,            0,               0,           -1 },
+  { "Alacritty",  NULL,   NULL,       1 << 3,         0,            1,               0,           -1 },
+  { "imv",        NULL,   NULL,       1 << 4,         0,            0,               0,           -1 },
+  { "Virtualbox", NULL,   NULL,       1 << 5,         1,            0,               0,           -1 },
+  { "ranger",     NULL,   NULL,       1 << 1,         0,            1,               0,           -1 },
+  { "PCManFM",     NULL,   NULL,      1 << 1,         0,            1,               0,           -1 },
   { "trayer",     NULL,   NULL,       ~0,             0,            0,               0,           -1 },
   { "slstatus",   NULL,   NULL,       ~0,             0,            0,               0,           -1 },
   { "Steam",      NULL,   NULL,       1 << 2,         1,            0,               0,           -1 },
@@ -59,7 +66,7 @@ static const Rule rules[] = {
 };
 
 /* layout(s) */
-static const float mfact     = 0.6; /* factor of master area size [0.05..0.95] */
+static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 2;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 
@@ -68,8 +75,8 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "[\\]",     dwindle }, /* first entry is default */
-	{ "[]=",      tile },   
+	{ "[]=",      tile }, /* first entry is default */
+	{ "[\\]",     dwindle },
 	{ "[M]",      monocle },
 	{ "[@]",      spiral },
 	{ "H[]",      deck },
@@ -107,9 +114,10 @@ static const char *termcmd[]  = { "alacritty", NULL };
 static const char *layoutmenu_cmd = "layoutmenu.sh";
 static const char *flamegui[] = { "flameshot", "gui", NULL};
 static const char *flamefull[] = { "flameshot", "full", "-c", NULL };
-static const char *browser[] = { "qutebrowser", NULL };
-static const char *ranger[] = { "alacritty", "-e", "ranger", NULL };
+static const char *browser[] = { "firefox", NULL };
+static const char *files[] = { "pcmanfm", NULL };
 static const char *trayer[] = { "trayer", "--expand", "true", "--widthtype", "request", "--transparent", "true", "--alpha", "255", "--edge", "top", "--align", "right" };
+static const char *killteams[] = { "killall", "teams", NULL };
 
 #include "movestack.c"
 static Key keys[] = {
@@ -124,8 +132,9 @@ static Key keys[] = {
   { MODKEY|ShiftMask,             XK_s,      spawn,          {.v = flamegui } },
   { MODKEY|ShiftMask,             XK_z,      spawn,          {.v = trayer } },
   { MODKEY,                       XK_p,      spawn,          {.v = flamefull } },
-  { MODKEY,                       XK_x,      spawn,          {.v = ranger } },
+  { MODKEY,                       XK_x,      spawn,          {.v = files } },
   { MODKEY,                       XK_e,      spawn,          {.v = browser} },
+  { MODKEY|ControlMask,           XK_w,      spawn,          {.v = killteams } },
 	{ MODKEY|ControlMask,		XK_comma,  cyclelayout,    {.i = -1 } },
 	{ MODKEY|ControlMask,           XK_period, cyclelayout,    {.i = +1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
@@ -137,22 +146,22 @@ static Key keys[] = {
   { MODKEY|ControlMask,              XK_bracketright,     incnmaster,     {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY|ShiftMask,                       XK_Return, zoom,           {0} },
-	{ MODKEY|Mod1Mask,              XK_u,      incrgaps,       {.i = +1 } },
-	{ MODKEY|Mod1Mask|ShiftMask,    XK_u,      incrgaps,       {.i = -1 } },
-	{ MODKEY|Mod1Mask,              XK_i,      incrigaps,      {.i = +1 } },
-	{ MODKEY|Mod1Mask|ShiftMask,    XK_i,      incrigaps,      {.i = -1 } },
-	{ MODKEY|Mod1Mask,              XK_o,      incrogaps,      {.i = +1 } },
-	{ MODKEY|Mod1Mask|ShiftMask,    XK_o,      incrogaps,      {.i = -1 } },
-	{ MODKEY|Mod1Mask,              XK_6,      incrihgaps,     {.i = +1 } },
-	{ MODKEY|Mod1Mask|ShiftMask,    XK_6,      incrihgaps,     {.i = -1 } },
-	{ MODKEY|Mod1Mask,              XK_7,      incrivgaps,     {.i = +1 } },
-	{ MODKEY|Mod1Mask|ShiftMask,    XK_7,      incrivgaps,     {.i = -1 } },
-	{ MODKEY|Mod1Mask,              XK_8,      incrohgaps,     {.i = +1 } },
-	{ MODKEY|Mod1Mask|ShiftMask,    XK_8,      incrohgaps,     {.i = -1 } },
-	{ MODKEY|Mod1Mask,              XK_9,      incrovgaps,     {.i = +1 } },
-	{ MODKEY|Mod1Mask|ShiftMask,    XK_9,      incrovgaps,     {.i = -1 } },
-	{ MODKEY|Mod1Mask,              XK_0,      togglegaps,     {0} },
-	{ MODKEY|Mod1Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
+	{ MODKEY|ControlMask,              XK_u,      incrgaps,       {.i = +1 } },
+	{ MODKEY|ControlMask|ShiftMask,    XK_u,      incrgaps,       {.i = -1 } },
+	{ MODKEY|ControlMask,              XK_i,      incrigaps,      {.i = +1 } },
+	{ MODKEY|ControlMask|ShiftMask,    XK_i,      incrigaps,      {.i = -1 } },
+	{ MODKEY|ControlMask,              XK_o,      incrogaps,      {.i = +1 } },
+	{ MODKEY|ControlMask|ShiftMask,    XK_o,      incrogaps,      {.i = -1 } },
+	{ MODKEY|ControlMask,              XK_6,      incrihgaps,     {.i = +1 } },
+	{ MODKEY|ControlMask|ShiftMask,    XK_6,      incrihgaps,     {.i = -1 } },
+	{ MODKEY|ControlMask,              XK_7,      incrivgaps,     {.i = +1 } },
+	{ MODKEY|ControlMask|ShiftMask,    XK_7,      incrivgaps,     {.i = -1 } },
+	{ MODKEY|ControlMask,              XK_8,      incrohgaps,     {.i = +1 } },
+	{ MODKEY|ControlMask|ShiftMask,    XK_8,      incrohgaps,     {.i = -1 } },
+	{ MODKEY|ControlMask,              XK_9,      incrovgaps,     {.i = +1 } },
+	{ MODKEY|ControlMask|ShiftMask,    XK_9,      incrovgaps,     {.i = -1 } },
+	{ MODKEY|ControlMask,              XK_0,      togglegaps,     {0} },
+	{ MODKEY|ControlMask|ShiftMask,    XK_0,      defaultgaps,    {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
